397692cebb99b233ce16503a3e4feec110e81391
Sok Pomaranczowy <sok_pomaranczowy>
2017-03-19 10:21:38 +0100

    [REQ-1] Added products to vending machine

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -1,5 +1,17 @@
 package tdd.vendingMachine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VendingMachine {
 
+    private List<String> products;
+
+    public VendingMachine() {
+        this.products = new ArrayList<>();
+    }
+
+    public List<String> getProducts() {
+        return products;
+    }
 }
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,12 +1,25 @@
 package tdd.vendingMachine;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class VendingMachineTest {
 
+    public VendingMachine vendingMachine;
+
+    @Before
+    public void onSetUp(){
+        this.vendingMachine = new VendingMachine();
+    }
+
     @Test
-    public void just_a_stupid_passing_test_to_ensure_that_tests_are_run() {
-        Assertions.assertThat(new VendingMachine()).isNotNull();
+    public void given_stupid_passing_test_ensure_that_tests_are_run() {
+        Assertions.assertThat(vendingMachine).isNotNull();
+    }
+
+    @Test
+    public void givenVendingMachineThenProductsNotEmpty(){
+        Assertions.assertThat(vendingMachine.getProducts()).isNotNull();
     }
 }
